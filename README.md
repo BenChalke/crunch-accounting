6ebfb7ed5ee36a20f3b084497cfcaa2061ca2041
Benjamin Chalke <benjamin.chalke@intygrate.com>
2020-03-16 21:58:39 +1100

    Created a base project

---
+++ README.md
@@ -0,0 +1,12 @@
+# Crunch Accounting 
+
+To get started you will need to clone the project.
+
+You will then need to run:
+
+npm install
+
+After you have done this you will be able to start up your local server by running: 
+
+npm run serve
+
---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import CrunchAccounting from "./components/CrunchAccounting";
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+
+ReactDOM.render(<CrunchAccounting />, document.getElementById("app"));
